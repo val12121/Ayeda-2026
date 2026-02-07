ef3fc866fdabaec654e7321d431214a243978e80
Camila Valentina Alonso Yepez <114765967+val12121@users.noreply.github.com>
2026-02-07 21:44:11 +0000

    Primeros pasos, clase Tape

---
+++ Tape.h
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+
+using namespace std; 
+
+class Tape {
+  private:
+    bool color; // color == true ? black : white 
+    int sizeX;
+    int sizeY;
+
+    vector<bool> tape_; 
+    
+
+  public:
+    Tape() {
+      
+    }
+};
