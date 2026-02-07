9476a3f9c74dd84831f75d9be29431a91af1addf
Camila Valentina Alonso Yepez <114765967+val12121@users.noreply.github.com>
2026-02-07 23:24:35 +0000

    tape.h arreglado

--- Tape.h
+++ Tape.h
@@ -5,15 +5,25 @@ using namespace std;
 
 class Tape {
   private:
-    bool color; // color == true ? black : white 
-    int sizeX;
-    int sizeY;
-
-    vector<bool> tape_; 
-    
+    bool color_= false; // color == true ? black : white 
+    int sizeX_ = 20;
+    int sizeY_ = 20;
+    vector<vector<bool>> tape_; 
 
   public:
-    Tape() {
-      
+    Tape() {};
+    Tape(int sizeX, int sizeY) { 
+      sizeX_ = sizeX; 
+      sizeY_ = sizeY;
+
+      tape_[sizeX][sizeY];
     }
+  
+    bool get_color() { return color_; };
+    vector<vector<bool>> get_tape() { return tape_; };
+
+    void set_color(bool color) { color_ = color; } 
+    void InfoCell(int posX, int posY) {
+      std::cout << "The cell is " << get_tape()[posX][posY] << endl;
+    };
 };
---
+++ main.cc
@@ -0,0 +1,8 @@
+#include "tape.h"
+
+int main () {
+  Tape tape;
+  int i, j = 10;
+  std::cout << tape.get_tape()[i][j];
+  
+}
