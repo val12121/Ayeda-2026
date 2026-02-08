d44da38b386416695cde5a025830ae15aae9e9a8
Camila Valentina Alonso Yepez <114765967+val12121@users.noreply.github.com>
2026-02-08 12:47:38 +0000

    Por lo menos compila

--- ant.h
+++ ant.h
@@ -3,7 +3,7 @@
 
 #include "Tape.h"
 
-enum class Direction { Left, Right, Up, Down };
+enum class Direction { Left, Up, Right, Down };
 
 class Ant {
  public:
@@ -26,9 +26,41 @@ class Ant {
   Direction dir_;
   void turnLeft() { 
     if ( static_cast<int>(dir_) >= 1) { 
+      dir_ = static_cast<Direction>(static_cast<int>(dir_) - 1);
+    } else { dir_ = static_cast<Direction>(4); }
+  }
+
+  void turnRight() {
+    if ( static_cast<int>(dir_) <= 3) { 
       dir_ = static_cast<Direction>(static_cast<int>(dir_) + 1);
-    } //else { dir_ =  }   
-  };
-  void turnRight();
-  void move();
+    } else { dir_ = static_cast<Direction>(1); }
+  }
+
+  //Setters
+  void setx( int x ) { x_ = x; }
+  void sety( int y ) { y_ = y; }
+
+  //Getters
+  int getx() const { return x_; }
+  int gety() const { return y_; }
+
+  void move() {
+    int dir1 = static_cast<int>(dir_);
+    switch (dir1) {
+      case 0: 
+          setx(getx() - 1);
+        break;
+      case 1:
+          sety(gety() + 1);
+        break;
+      case 2:
+          setx(getx() + 1);
+        break;
+      case 3:
+          sety(gety() - 1);
+        break;
+      default:
+        break;
+    }
+  }
 };
--- main.cc
+++ main.cc
@@ -1,8 +1,10 @@
-#include "tape.h"
+#include "ant.h"
 
 int main () {
   Tape tape;
   int i, j = 10;
   tape.InfoCell(i, j);
+
+  Ant ant (5, 5, static_cast<Direction>(1)); 
 
 }
